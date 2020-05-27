d14f9fa243f2a15f36a6cfc4e4f21b191c19a4c4
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-27 10:16:50 -0700

    add new tests to albums_spec. Include new before_save method in album.rb.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Album do
   it { should have_many(:songs) }
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+  
+  it("titleizes the name of an album") do
+    album = Album.create({name: "abyssal", genre: "Death Metal"})
+    expect(album.name()).to(eq("Abyssal"))
+  end
 end
